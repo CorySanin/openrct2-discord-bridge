7c91e03b7bb00a16db84b2962bc2d0eb5a11ee9b
Cory Sanin <corysanin@outlook.com>
2023-04-09 01:20:11 -0500

    Translate Discord stickers to text Also insignificant discord.js version bump

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ if (typeof (registerPlugin) === "undefined") {
                         type: 'chat',
                         body: {
                             author: msg.author.username,
-                            content: emoji.emojiToText(msg.content)
+                            content: msg.stickers.size ? `sent a sticker- "${msg.stickers.first().name}"` : emoji.emojiToText(msg.content)
                         }
                     };
                     for (let conId in connections) {
